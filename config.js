f9b3d8301df53a999e8f6a8710b328bc29d5ecef
DarkWorld03 <jorgeadriel03@gmail.com>
2026-05-26 18:34:35 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const GITHUB_TOKEN = "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";
-const REPO_OWNER = "DarkWorld03";
-const REPO_NAME = "milenium-store";
-const CLOUDINARY_CLOUD_NAME = "dtjb4m3ij";
-const CLOUDINARY_UPLOAD_PRESET = "milenium_preset";
-
-const GITHUB_API_URL = `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/productos.json`;
-const CLOUDINARY_UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`;
