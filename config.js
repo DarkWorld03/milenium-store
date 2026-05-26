ee4c20576b7bb0f7db5e2eb66300d93e31b44e88
DarkWorld03 <jorgeadriel03@gmail.com>
2026-05-26 17:51:12 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,3 +3,6 @@ const REPO_OWNER = "DarkWorld03";
 const REPO_NAME = "milenium-store";
 const CLOUDINARY_CLOUD_NAME = "dtjb4m3ij";
 const CLOUDINARY_UPLOAD_PRESET = "milenium_preset";
+
+const GITHUB_API_URL = `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/productos.json`;
+const CLOUDINARY_UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`;
