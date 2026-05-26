bc1a718bc104751009d52ca59e88f8225518bdad
DarkWorld03 <jorgeadriel03@gmail.com>
2026-05-26 17:43:01 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const GITHUB_TOKEN = "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";
+const REPO_OWNER = "DarkWorld03";
+const REPO_NAME = "milenium-store";
+const CLOUDINARY_CLOUD_NAME = "dtjb4m3ij";
+const CLOUDINARY_UPLOAD_PRESET = "milenium_preset";
