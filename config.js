ab621dd13ce296044d182e7900435075920f9a54
DarkWorld03 <jorgeadriel03@gmail.com>
2026-05-26 20:26:58 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const GITHUB_TOKEN = "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";
+const REPO_OWNER = "DarkWorld03";
+const REPO_NAME = "milenium-store";
+const CLOUDINARY_CLOUD_NAME = "dtjb4m3ij";
+const CLOUDINARY_UPLOAD_PRESET = "milenium_preset";
+
+const GITHUB_API_URL = "https://api.github.com/repos/" + REPO_OWNER + "/" + REPO_NAME + "/contents/productos.json";
+const CLOUDINARY_UPLOAD_URL = "https://api.cloudinary.com/v1_1/" + CLOUDINARY_CLOUD_NAME + "/image/upload";
